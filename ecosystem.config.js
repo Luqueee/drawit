9eb51ef67fb16932acd45db76630d9888b3d7ba4
luqueee <luqueee_@outlook.es>
2025-09-12 22:12:03 +0200

    Remove exec_mode and instances configuration from ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,6 @@ module.exports = {
   apps: [
     {
       name: "RPLACE-WEB",
-      exec_mode: "cluster",
-      instances: "max", // Or a number of instances
       script: "node_modules/next/dist/bin/next",
       args: "start",
       autorestart: true,
