ade3a9c9e81c91b06d60493af54b6e0badd3d593
luqueee <luqueee_@outlook.es>
2025-09-19 10:46:26 +0200

    feat: configure sitemap settings and enable pre/post scripts for improved SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,25 +3,28 @@ const disallowedPaths = [];
 
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: baseUrl,
+  siteUrl: "https://drawit.place",
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  changefreq: "weekly",
-  priority: 0.7,
-  robotsTxtOptions: {
-    policies: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: disallowedPaths,
-      },
-      {
-        userAgent: "Googlebot-Image",
-        allow: ["/images/*"],
-      },
-    ],
-    // additionalSitemaps: [`${baseUrl}/sitemap.xml`],
-    sitemap: `${baseUrl}/sitemap.xml`,
+  // generateIndexSitemap: true,
+  priority: 0.8,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  transform: async (config, path) => {
+    let priority = config.priority;
+    let changefreq = config.changefreq;
+    // Set higher priority for home and team pages
+    if (path === "/") {
+      priority = 1.0; // Highest priority for the homepage
+      changefreq = "hourly"; // Change frequency for the homepage, hourly is just an example consult the sitemap documentation or your SEO expert
+    }
+
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: priority, // Dynamic priority based on the page
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
   },
-  exclude: disallowedPaths,
 };
